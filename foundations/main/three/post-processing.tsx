5535b294a75b63969b5518a6e0301f191550d937
ericggul <57379903+ericggul@users.noreply.github.com>
2024-04-12 23:41:14 +0900

    Fix: Deploy Bug Fix

--- foundations/main/three/post-processing.tsx
+++ foundations/main/three/post-processing.tsx
@@ -5,21 +5,9 @@ import { useMemo } from "react";
 const THRESHOLD = 1;
 
 export default function PostProcessing({ scrollPos }: any) {
-  const bloomIntensity = useMemo(() => {
-    if (scrollPos < 0.25) return 2;
-    else if (scrollPos > 0.4) return -2;
-    else {
-      return 2 - (scrollPos - 0.25) * (4 / 0.15);
-    }
-  }, [scrollPos]);
-
   return (
     <>
-      <EffectComposer>
-        {/* <Bloom intensity={bloomIntensity} luminanceThreshold={0} luminanceSmoothing={0} height={400} /> */}
-        {scrollPos >= 0.98 && scrollPos <= 0.999 && <Glitch duration={[0.1, 1]} threshold={THRESHOLD} />}
-      </EffectComposer>
-      {/* {scrollPos >= 0.95 && scrollPos <= 0.999 && <CameraShake intensity={100 * (scrollPos - 0.95)} yawFrequency={1} pitchFrequency={1} rollFrequency={1} triggerThreshold={0.5} active />} */}
+      <EffectComposer>{scrollPos >= 0.98 && scrollPos <= 0.999 ? <Glitch duration={[0.1, 1]} threshold={THRESHOLD} /> : null}</EffectComposer>
     </>
   );
 }
