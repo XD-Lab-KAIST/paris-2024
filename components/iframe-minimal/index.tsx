4519b10e5770c134eed647b2c1ad105456d4afa6
ericggul <57379903+ericggul@users.noreply.github.com>
2024-10-30 15:39:19 +0900

    Fix: Values

--- components/iframe-minimal/index.tsx
+++ components/iframe-minimal/index.tsx
@@ -6,22 +6,23 @@ import * as S from "./styles";
 const LINK = "https://uncharted-territory-xdlab.vercel.app/";
 
 export default function MainComp() {
-  const [heightVh, setHeightVh] = useState(60);
-  const [topVh, setTopVh] = useState(2);
+  const [heightVh, setHeightVh] = useState(55.5);
+  const [topVh, setTopVh] = useState(5.1);
+  const [widthVw, setWidthVw] = useState(78.9);
+  const [leftVw, setLeftVw] = useState(6.1);
 
   return (
     <S.Wrapper>
-      <S.IFrame src={LINK} frameBorder="0" style={{ height: `${heightVh}vh`, top: `${topVh}vh` }} />
-      {/* <S.DebugControls>
-        <div>
-          <label>Height (vh): {heightVh}</label>
-          <input type="range" min="0" max="100" value={heightVh} onChange={(e) => setHeightVh(Number(e.target.value))} />
-        </div>
-        <div>
-          <label>Top Position (vh): {topVh}</label>
-          <input type="range" min="0" max="100" value={topVh} onChange={(e) => setTopVh(Number(e.target.value))} />
-        </div>
-      </S.DebugControls> */}
+      <S.IFrame
+        src={LINK}
+        frameBorder="0"
+        style={{
+          height: `${heightVh}vh`,
+          top: `${topVh}vh`,
+          width: `${widthVw}vw`,
+          left: `${leftVw}vw`,
+        }}
+      />
     </S.Wrapper>
   );
 }
